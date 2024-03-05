ae2ea9a1a65bc60d6612c6d8f2d871f859469373
Shavidika <shavidika.ekanayake@gmail.com>
2024-03-05 12:30:36 +0530

    third

---
+++ frontend/src/config.js
@@ -0,0 +1,19 @@
+// ==============================|| THEME CONFIG  ||============================== //
+
+const config = {
+  defaultPath: '/dashboard/default',
+  fontFamily: `'Public Sans', sans-serif`,
+  i18n: 'en',
+  miniDrawer: false,
+  container: true,
+  mode: 'light',
+  presetColor: 'theme5',
+  themeDirection: 'ltr'
+};
+
+export default config;
+export const drawerWidth = 260;
+
+export const twitterColor = '#000000';
+export const facebookColor = '#3b5998';
+export const linkedInColor = '#0e76a8';
