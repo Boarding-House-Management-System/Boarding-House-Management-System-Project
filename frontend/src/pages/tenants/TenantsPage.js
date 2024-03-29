b94b038590556a6b71f2793c50c0c841207637df
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-03-30 02:22:19 +0530

    Search a Tenant | Add a New Tenant       Forms

--- frontend/src/pages/tenants/TenantsPage.js
+++ frontend/src/pages/tenants/TenantsPage.js
@@ -1,20 +1,75 @@
 // material-ui
 import { Typography } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 // project import
-import MainCard from 'components/MainCard';
+import { Button, Grid, Paper, TextField } from '../../../node_modules/@mui/material/index';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const TenantsPage = () => (
-  <MainCard title="Tenants">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
+  <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+    {/* Search A Tenant */}
+    <Grid item xs={12} md={12} lg={3}>
+      <Paper sx={{ pl: 4, pr: 4 }}>
+        <Typography variant="h5" sx={{ pt: 2 }}>
+          Search a Tenant
+        </Typography>
+        <Typography sx={{ pt: 2 }}>Please add the tenant’s NIC </Typography>
+        <form>
+          <TextField sx={{ mt: 2 }} id="TenantNIC" label="Tenant’s NIC " variant="outlined" />
+        </form>
+        <Button sx={{ mt: 2, mb: 2 }} variant="contained">
+          Search
+        </Button>
+      </Paper>
+    </Grid>
+
+    {/* Add A Tenant */}
+    <Grid item xs={12} md={12} lg={9}>
+      <Paper sx={{ pl: 4, pr: 4 }}>
+        <Typography variant="h5" sx={{ pt: 2 }}>
+          Add a Tenant
+        </Typography>
+        <Typography sx={{ pt: 2 }}>Please generate a token </Typography>
+
+        {/* Token Generation */}
+        <div>
+          <Button variant="outlined" sx={{ m: 2 }}>
+            Generate a Token
+          </Button>
+          <TextField
+            sx={{ mt: 2 }}
+            disabled
+            id="Token"
+            label="Token "
+            variant="outlined"
+            defaultValue="https://www.google.com/search?q=to"
+          />
+          <Button sx={{ m: 2, ml: 0 }}>
+            <ContentCopyIcon />
+          </Button>
+        </div>
+
+        {/* Tenants Details        */}
+        <Typography sx={{ pt: 2 }}>Send generated token to: </Typography>
+        <form>
+          <TextField
+            fullWidth
+            sx={{ mt: 2 }}
+            required
+            id="TenantEmail"
+            label="Tenant’s Email "
+            variant="outlined"
+            defaultValue="abc@gmail.com"
+          />
+        </form>
+        <Button sx={{ mt: 2, mb: 2 }} variant="contained">
+          Send
+        </Button>
+      </Paper>
+    </Grid>
+  </Grid>
 );
 
 export default TenantsPage;
